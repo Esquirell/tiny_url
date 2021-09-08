57936047c4441271aa1b812cf8b9a0f99c9535de
Dmitry Kuksin <frantomas6677@gmail.com>
2021-09-08 18:18:33 +0300

    Added schedule for URL model

--- app/Models/Url.php
+++ app/Models/Url.php
@@ -2,10 +2,24 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Prunable;
 
 class Url extends Model
 {
-    use HasFactory;
+    use HasFactory, Prunable;
+
+    protected $fillable = [
+        'url',
+        'redirect_url',
+        'lifetime',
+        'transition'
+    ];
+
+    public function prunable()
+    {
+        return static::where('lifetime', '<=', Carbon::now()->addHours(3));
+    }
 }
